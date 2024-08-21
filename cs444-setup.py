198aa9d5386f0ecc55ff0cd63e93aaa5555b0210
Patrick Iacob <iacobp@oregonstate.edu>
2024-08-21 08:46:51 -0700

    update setup script to python 3

--- cs444-setup.py
+++ cs444-setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import argparse
 import os
@@ -6,13 +6,13 @@ import shutil
 import subprocess
 
 HOME = os.getenv("HOME")
-CS444_BASE = "%s/.cs444" % HOME
+CS444_BASE = f"{HOME}/.cs444"
 
 def ask_intent(string):
     answer = 'z'
-    while (answer != 'y' and answer != 'n'):
+    while answer not in ('y', 'n'):
         print(string + " (y/n) ?")
-        answer = raw_input().strip().lower()
+        answer = input().strip().lower()
 
     return answer == 'y'
 
@@ -21,18 +21,19 @@ def backup_and_write_file(fn, data):
     if os.path.exists(fn):
         for i in range(1000):
             # fn.bak.[number] is backup
-            backup_fn = '%s.bak.%d' % (fn, i)
+            backup_fn = f'{fn}.bak.{i}'
             if not os.path.exists(backup_fn):
                 shutil.copyfile(fn, backup_fn)
-		break
+                break
     with open(fn, 'w') as f:
         f.write(data)
 
 def clone():
+    os.system('git config --global core.autocrlf false')
     if os.path.exists(CS444_BASE):
-	os.system("rm -rf %s" % CS444_BASE)
+        os.system(f"rm -rf {CS444_BASE}")
     try:
-        r = os.system('git clone https://github.com/blue9057/peda %s' % CS444_BASE)
+        r = os.system(f'git clone https://github.com/stackunderfl0w/peda {CS444_BASE}')
     except:
         print("Cloning cs444 git repository has failed!")
         return
@@ -44,33 +45,32 @@ def clone():
 def install_gdb():
     if not ask_intent("Do you want to install peda to ~/.gdbinit"):
         return
-    with open("%s/gdbinit" % CS444_BASE,'rb') as f:
+    with open(f"{CS444_BASE}/gdbinit", 'r') as f:
         gdbinit = f.read()
-    backup_and_write_file('%s/.gdbinit' % HOME, gdbinit)
+    backup_and_write_file(f'{HOME}/.gdbinit', gdbinit)
 
 def install_bash():
     if not ask_intent("Do you want to install .bashrc"):
         return
 
-    with open("%s/bashrc" % CS444_BASE,'rb') as f:
+    with open(f"{CS444_BASE}/bashrc", 'r') as f:
         bashrc = f.read()
-    backup_and_write_file('%s/.bashrc' % HOME, bashrc)
+    backup_and_write_file(f'{HOME}/.bashrc', bashrc)
 
-    cshrc = None
-    with open("%s/.cshrc" % HOME, 'rb') as f:
-        cshrc = f.read()
-        if not 'exec /bin/bash' in cshrc:
-            cshrc += "\nexec /bin/bash\n"
-    if cshrc != None:
-        backup_and_write_file('%s/.cshrc' % HOME, cshrc)
+    if os.path.exists(f"{HOME}/.cshrc"):
+        with open(f"{HOME}/.cshrc", 'r') as f:
+            cshrc = f.read()
+            if 'exec /bin/bash' not in cshrc:
+                cshrc += "\nexec /bin/bash\n"
+        backup_and_write_file(f'{HOME}/.cshrc', cshrc)
 
 def install_vim():
     if not ask_intent("Do you want to install .vimrc and vim plugins"):
         return
 
-    with open("%s/vimrc" % CS444_BASE, 'rb') as f:
+    with open(f"{CS444_BASE}/vimrc", 'r') as f:
         vimrc = f.read()
-    backup_and_write_file('%s/.vimrc' % HOME, vimrc)
+    backup_and_write_file(f'{HOME}/.vimrc', vimrc)
 
     os.system("vim +PlugInstall +qall")
 
@@ -78,9 +78,9 @@ def install_tmux():
     if not ask_intent("Do you want to install cs444 custom tmux configuration"):
         return
 
-    with open("%s/tmux.conf" % CS444_BASE, 'rb') as f:
+    with open(f"{CS444_BASE}/tmux.conf", 'r') as f:
         tmux_conf = f.read()
-    backup_and_write_file('%s/.tmux.conf' % HOME, tmux_conf)
+    backup_and_write_file(f'{HOME}/.tmux.conf', tmux_conf)
 
 def copy_qemu():
     copy_file_to_bin('qemu-system-i386')
@@ -89,13 +89,11 @@ def copy_qemu():
     copy_file_to_bin('ta-help')
 
 def copy_file_to_bin(name):
-    if not os.path.exists("%s/bin" % HOME):
-        os.makedirs("%s/bin" % HOME)
+    if not os.path.exists(f"{HOME}/bin"):
+        os.makedirs(f"{HOME}/bin")
 
-    shutil.copyfile("%s/bin/%s" % (CS444_BASE, name),
-            "%s/bin/%s" % (HOME, name))
-    os.system("chmod +x %s/bin/%s" % (HOME, name))
-
+    shutil.copyfile(f"{CS444_BASE}/bin/{name}", f"{HOME}/bin/{name}")
+    os.system(f"chmod +x {HOME}/bin/{name}")
 
 def main():
     clone()
